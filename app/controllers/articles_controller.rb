54a1d031600cb2b4e97c608c4c1a4b600fe81c34
cloudy9101 <cloudy9101@126.com>
2014-10-09 11:08:28 +0800

    finish upload-article function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,27 @@
 class ArticlesController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create]
   def index
   end
 
   def show
   	@article = Article.find(params[:id])
   end
+
+  def new
+  	@article = Article.new
+  end
+
+  def create
+  	@article = current_user.articles.build(article_params)
+        if @article && @article.save
+  	       redirect_to article_path(@article)
+        else
+              render 'new'
+        end
+  end
+
+  private
+  	def article_params
+  		params.require(:article).permit(:avatar, :title, :remark, :category_id, :source, :vid)
+  	end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,12 @@ class Article < ActiveRecord::Base
 	belongs_to :category
 	belongs_to :user
 	mount_uploader :avatar, AvatarUploader
+
+	validates :title, :remark, :category, :source, :vid, :avatar, presence: true
+	validates :title, length: { maximum: 50 }
+	validates :remark, length: { maximum: 200 }
+	validates :category, exclusion: { in: [1, 2, 3], message: "分类不存在" }
+	validates :source, exclusion: { in: %w( youku sina qq ), message: "您所选择的视频源暂不支持" }
+	validates :title, :vid, uniqueness: true
+
 end
